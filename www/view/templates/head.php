851a39a116076bb186a90ab98f57ea130805354c
watahiki <0715yansan@gmail.com>
2020-03-18 20:22:59 +0900

    com

--- www/view/templates/head.php
+++ www/view/templates/head.php
@@ -1,6 +1,10 @@
+<!--文字エンコーディングをUTF-8に指定-->
 <meta charset="UTF-8">
+<!--viewport(画面の表示領域)の幅をdevice-width(デバイスごとの幅)にinitial-scale(初期倍率)を1倍(100%)に指定-->
 <meta name="viewport" content="width=device-width,initial-scale=1">
+<!--X-UA-Compatible（IE互換モード）　ie(インターネットエクスプローラー)をedge(最新バージョン)で表示する-->
 <meta http-equiv="X-UA-Compatible" content="ie=edge">
+<!--cssファイル読み込み  integrity(正常な状態のファイルのハッシュ値(同じデータなら同じハッシュ値)を指定)  crossorigin-->
 <link rel="stylesheet" href="//stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 <script src="//code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="//cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
