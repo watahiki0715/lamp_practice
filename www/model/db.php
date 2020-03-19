6008d31d8cb3f098b78e0c6c59d74e9a165d1c10
watahiki <0715yansan@gmail.com>
2020-03-19 23:45:01 +0900

    com

--- www/model/db.php
+++ www/model/db.php
@@ -7,9 +7,13 @@ function get_db_connect(){
  
   try {
     // データベースに接続
+    //データベースの情報を指定
     $dbh = new PDO($dsn, DB_USER, DB_PASS, array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8mb4'));
+    //エラーモードの設定
     $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    //プリペアドステートメントの設定
     $dbh->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+    //
     $dbh->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
   } catch (PDOException $e) {
     exit('接続できませんでした。理由：'.$e->getMessage() );
@@ -17,17 +21,23 @@ function get_db_connect(){
   return $dbh;
 }
 
+//DBのレコード取得
 function fetch_query($db, $sql, $params = array()){
   try{
+    // SQL文を実行する準備
     $statement = $db->prepare($sql);
+    // SQLを実行
     $statement->execute($params);
+    // レコードの取得
     return $statement->fetch();
+   //function.phpのset_error関数によりエラーを追加
   }catch(PDOException $e){
     set_error('データ取得に失敗しました。');
   }
   return false;
 }
 
+//
 function fetch_all_query($db, $sql, $params = array()){
   try{
     $statement = $db->prepare($sql);
