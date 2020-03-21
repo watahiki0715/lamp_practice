b54146530f2f07a483ac3175808b6e96b185c05b
watahiki <0715yansan@gmail.com>
2020-03-21 15:56:31 +0900

    #5

--- www/view/templates/messages.php
+++ www/view/templates/messages.php
@@ -1,8 +1,8 @@
 <!--エラーの配列を取得して全て表示-->
 <?php foreach(get_errors() as $error){ ?>
-  <p class="alert alert-danger"><span><?php print $error; ?></span></p>
+  <p class="alert alert-danger"><span><?php print(h($error)); ?></span></p>
 <?php } ?>
 <!--メッセージの配列を取得して全て表示-->
 <?php foreach(get_messages() as $message){ ?>
-  <p class="alert alert-success"><span><?php print $message; ?></span></p>
+  <p class="alert alert-success"><span><?php print(h($message)); ?></span></p>
 <?php } ?>
